3ed6350ee813ddf404959d3884de7ec6e697317d
seo-sang-ho <rocter2049@naver.com>
2024-08-21 16:03:27 +0900

    feat: getUsername from cookie

---
+++ src/main/java/article/projectNum1/ControllerAdvice.java
@@ -0,0 +1,24 @@
+package article.projectNum1;
+
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+
+@org.springframework.web.bind.annotation.ControllerAdvice
+
+public class ControllerAdvice {
+
+	@ModelAttribute("username")
+	public String getUsernameFromCookies(HttpServletRequest request) {
+		Cookie[] cookies = request.getCookies();
+		if (cookies != null) {
+			for (Cookie cookie : cookies) {
+				if ("username".equals(cookie.getName())) {
+					return cookie.getValue();
+				}
+			}
+		}
+		return null; // 로그인되지 않은 경우 null 반환
+	}
+}
