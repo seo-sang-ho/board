3b429cb3debc07def7e84ffc1f06698ed304e912
seo-sang-ho <rocter2049@naver.com>
2024-08-23 12:12:43 +0900

    메인 페이지를 글 목록으로 변경

--- src/main/java/article/projectNum1/HomeController.java
+++ src/main/java/article/projectNum1/HomeController.java
@@ -1,13 +1,25 @@
 package article.projectNum1;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import article.projectNum1.write.entity.Write;
+import article.projectNum1.write.service.WriteService;
+import lombok.RequiredArgsConstructor;
+
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
 
+	private final WriteService writeService;
+
 	@GetMapping("/")
-	public String mainPage(){
-		return "index";
+	public String mainPage(Model model){
+		List<Write> writes = writeService.findAll();
+		model.addAttribute("writes", writes);
+		return "writes/writeList";
 	}
 }
