42ed068d65017c45005d12a0450a759a31ca7603
seo-sang-ho <rocter2049@naver.com>
2024-08-25 11:25:51 +0900

    로그인 기능 구현(세션 방식_ )

--- src/main/java/article/projectNum1/ControllerAdvice.java
+++ src/main/java/article/projectNum1/ControllerAdvice.java
@@ -2,21 +2,35 @@ package article.projectNum1;
 
 import org.springframework.web.bind.annotation.ModelAttribute;
 
+import article.projectNum1.member.entity.Member;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 
 @org.springframework.web.bind.annotation.ControllerAdvice
 
 public class ControllerAdvice {
 
+	//@ModelAttribute("username")
+	// public String getUsernameFromCookies(HttpServletRequest request) {
+	// 	Cookie[] cookies = request.getCookies();
+	// 	if (cookies != null) {
+	// 		for (Cookie cookie : cookies) {
+	// 			if ("username".equals(cookie.getName())) {
+	// 				return cookie.getValue();
+	// 			}
+	// 		}
+	// 	}
+	// 	return null; // 로그인되지 않은 경우 null 반환
+	// }
+
 	@ModelAttribute("username")
-	public String getUsernameFromCookies(HttpServletRequest request) {
-		Cookie[] cookies = request.getCookies();
-		if (cookies != null) {
-			for (Cookie cookie : cookies) {
-				if ("username".equals(cookie.getName())) {
-					return cookie.getValue();
-				}
+	public String getUsernameFromSession(HttpServletRequest request) {
+		HttpSession session = request.getSession(false);
+		if (session != null) {
+			Object username = session.getAttribute("loginMember");
+			if(username != null){
+				return ((Member) username).getUsername();
 			}
 		}
 		return null; // 로그인되지 않은 경우 null 반환
--- src/main/java/article/projectNum1/member/controller/LoginController.java
+++ src/main/java/article/projectNum1/member/controller/LoginController.java
@@ -10,7 +10,9 @@ import article.projectNum1.member.entity.Member;
 import article.projectNum1.member.service.LoginService;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
@@ -33,16 +35,23 @@ public class LoginController {
 	로그인 기능
 	 */
 	@PostMapping("/login")
-	public String login(@Valid LoginForm loginForm, Model model, HttpServletResponse response) {
+	public String login(@Valid LoginForm loginForm, Model model, HttpServletRequest request) {
 		Member loginMember = loginService.login(loginForm);
 		model.addAttribute("loginMember", loginMember);
 
 		if(loginMember == null){
 			throw new IllegalStateException("로그인 할 수 없습니다.");
 		}
+
+		/* 쿠키 로그인 방식
 		Cookie cookie = new Cookie("username", loginMember.getUsername());
 		cookie.setMaxAge(60 * 30); // 30분
 		response.addCookie(cookie);
+		 */
+
+		//세션 로그인 방식
+		HttpSession session = request.getSession();
+		session.setAttribute("loginMember",loginMember);
 
 		return "redirect:/";
 	}
@@ -51,10 +60,15 @@ public class LoginController {
 	로그아웃 기능
 	 */
 	@PostMapping("/logout")
-	public String logout(HttpServletResponse response) {
-		Cookie cookie = new Cookie("username",null);
-		cookie.setMaxAge(0);
-		response.addCookie(cookie);
+	public String logout(HttpServletRequest request) {
+		// Cookie cookie = new Cookie("username",null);
+		// cookie.setMaxAge(0);
+		// response.addCookie(cookie);
+
+		HttpSession session = request.getSession(false);
+		if(session != null){
+			session.invalidate(); // 세션 무효화
+		}
 		return "redirect:/";
 	}
 }
